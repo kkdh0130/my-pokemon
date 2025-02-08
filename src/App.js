7c71aef245119c6024e41d5f51262ca732f67f96
dahyun <kkdh0130@gmail.com>
2025-02-08 23:01:29 +0900

    Fix Router basename

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import Pokedex from './components/Pokedex';
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import "./App.css";
+import Pokedex from "./components/Pokedex";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PokemonDetailsPage from "./components/PokemonDetailsPage";
 import Layout from "./components/Layout";
 
 function App() {
   return (
-    <Router>  
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Pokedex />} />
@@ -16,7 +15,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
